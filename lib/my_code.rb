176261abbfda3f1166fe437a1ca7b38b0c8f0b92
Jack Cole <123jack.cole@gmail.com>
2020-01-17 02:20:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
-# My Code here....
+def map_to_negative(source_array)
+  counter = 0
+  while counter < source_array.length
+    source_array[counter] *= -1
+    counter += 1
+  end
+end
+
+def map_to_no_change(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+  new_array[counter] = source_array[counter]
+  end
+  new_array
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+
+end
