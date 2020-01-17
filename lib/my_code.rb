864640812facd3419985fc7bdc57b7696521f129
Jack Cole <123jack.cole@gmail.com>
2020-01-17 02:26:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  total = 0
+  counter = 0
+  while counter < source_array.length
+    
+  end
 end
 
 def reduce_to_all_true(source_array)
