83538fad10020736fe5d6c00bd225ba435d6c269
Jack Cole <123jack.cole@gmail.com>
2020-01-17 02:22:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negative(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length
-    new_array[counter] *= -1
+    new_array[counter] = source_array[counter] * -1
     counter += 1
   end
   new_array
