290925adf3df0ed739df7c3a7cb3bbe705c34f8c
Jack Cole <123jack.cole@gmail.com>
2020-01-17 02:24:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,11 +19,23 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+  new_array[counter] = source_array[counter] * 2
+  counter += 1
+  end
+  new_array
 end
 
 def map_to_square(source_array)
-  
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+  new_array[counter] = source_array[counter]**
+  counter += 1
+  end
+  new_array
 end
 
 def reduce_to_total(source_array, starting_point)
