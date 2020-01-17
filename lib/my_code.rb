436147aabe0f0a162c67a7546802ee9abb12c70b
Jack Cole <123jack.cole@gmail.com>
2020-01-17 02:25:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length
-  new_array[counter] = source_array[counter] **
+  new_array[counter] = source_array[counter] * source_array[counter] 
   counter += 1
   end
   new_array
